a1209bc2700ca652f294a2df32c3c9f255bb442f
MXd3v <84356551+MXd3v@users.noreply.github.com>
2021-06-21 14:18:35 -0300

    Add files via upload

---
+++ database.js
@@ -0,0 +1,14 @@
+const firebase = require("firebase");
+let Config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
+firebase.initializeApp(Config) //incia a database
+
+module.exports = firebase.database()
